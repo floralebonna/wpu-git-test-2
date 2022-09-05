e9f030bc8cb5d9dd96fdc1c741d4488f38cd5355
floralebonna <floralebonnakatumbo@gmail.com>
2022-09-06 01:07:52 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote Lagi</title>
+</head>
+<body>
+    <h1>Flora Lebonna Katumbo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam culpa obcaecati molestiae. Rem ea quis atque, quibusdam laudantium, quidem, porro molestias cum illum temporibus beatae eum explicabo reiciendis nulla. Maxime.</p>
+</body>
+</html>
